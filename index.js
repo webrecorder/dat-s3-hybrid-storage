6d34ef9a8af5c0c10defaedd6c1e995cf7167d71
Ilya Kreymer <ikreymer@users.noreply.github.com>
2018-07-30 13:01:39 -0700

    Develop->Master (#1)
    * api update: misc fixes and api update:
    - S3HybridStorage.create and constructor take an s3://bucket/path url
    - S3Importer can be constructed directly, need not take an S3HybridStorage instance
    - add debug logging for file import/skipping
    - ensure default chunk size is passed in
    - update README with new functionality
    
    * change import accessor to importer(), update README
    
    * README.md typo fix

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const AWS = require('aws-sdk')
 const DatStorage = require('dat-storage')
 const ras3 = require('random-access-s3')
 const S3Importer = require('./lib/importer')
+const { URL } = require('url')
 
 /**
  * @desc A Dat storage provider that provides dat metadata locally but content data from S3
@@ -14,30 +15,38 @@ class S3HybridStorage {
    * @param {string} s3prefix - Path to content in S3 bucket
    * @param {S3} s3           - AWS.S3 instance to use
    */
-  constructor (localDir, bucket, s3prefix, s3) {
+  constructor (s3Url, localDir, s3) {
+    s3Url = new URL(s3Url)
+    if (s3Url.protocol != "s3:") {
+      throw new Error("URL must be of the form s3://bucket/path")
+    }
+
     /**
      * @desc Prepended to filename argument of {@link loadFile}
      * @type {string}
      */
-    this.s3prefix = s3prefix
+    this.prefix = s3Url.pathname;
 
     /**
      * @desc The bucket to be used
      * @type {string}
      */
-    this.bucket = bucket
+    this.bucket = s3Url.hostname;
 
     /**
      * @desc AWS.S3 instance to use for loading content from the bucket
      * @type {S3}
      */
+    if (!s3) {
+      s3 = new AWS.S3({ apiVersion: '2006-03-01' })
+    }
     this.s3 = s3
 
     /**
      * @desc Utility class for importing files from s3
      * @type {S3Importer}
      */
-    this.importer = new S3Importer(this)
+    //this.importer = new S3Importer(this.bucket, this.prefix, s3)
 
     /**
      * @desc Storage provider for locally stored metadata
@@ -46,7 +55,7 @@ class S3HybridStorage {
     this.localStorage = DatStorage(localDir)
 
     this.loadFile = this.loadFile.bind(this)
-    this.storageProvider = this.storageProvider.bind(this)
+    //this.storageProvider = this.storageProvider.bind(this)
 
     /**
      * @desc Store used with custom loadFile to return s3 reader
@@ -59,35 +68,30 @@ class S3HybridStorage {
      * @type {{bucket: string, s3: S3}}
      */
     this.ras3Opts = {
-      bucket,
+      bucket: this.bucket,
       s3: this.s3
     }
   }
 
   /**
-   * @desc Import files metadata to local metadata store
+   * @desc Create S3Importer for importing files
    * @see {@link S3Importer.importFiles}
-   * @param {Hyperdrive} hyperdrive - The hyperdrive used for importing
-   * @return {Promise<Array<{to: string, err: Error}>>}
+   * @return {S3Importer}
    */
-  importFiles (hyperdrive) {
-    return this.importer.importFiles(hyperdrive)
+  importer() {
+    return new S3Importer(this.bucket, this.prefix, this.s3);
   }
 
   /**
    * @desc Create a new instance of S3HybridStorage and receive a storage {@link provider} for hyperdrive or dat-node
+   * @param {string} url - Url in the form s3://bucket/path/ where the data is stored
    * @param {string} localDir - Path to where the local metadata is stored
-   * @param {string} bucket   - Name of the S3 bucket where the content data is stored
-   * @param {string} s3prefix - Path to content in S3 bucket
    * @param {S3}    [s3]      - Optional AWS.S3 instance to use. Defaults to "new S3({ apiVersion: '2006-03-01' })"
    * @return {{metadata: function(string, Object), content: function(string, Object, hyperdrive)}}
    */
-  static create ({localDir, bucket, s3prefix, s3}) {
-    if (!s3) {
-      s3 = new AWS.S3({ apiVersion: '2006-03-01' })
-    }
-    const hs = new S3HybridStorage(localDir, bucket, s3prefix, s3)
-    return hs.provider()
+  static create (s3Url, localDir, s3) {
+    const hs = new S3HybridStorage(s3Url, localDir, s3)
+    return hs.storage()
   }
 
   /**
@@ -96,15 +100,15 @@ class S3HybridStorage {
    * @return {ras3} - random-access-s3 loader for the file
    */
   loadFile (filename) {
-    const path = this.s3prefix + filename
+    const path = this.prefix + filename
     return ras3(path, this.ras3Opts)
   }
 
   /**
-   * @desc Returns a custom storage provider that provides metadata for local directory but content data from S3
+   * @desc Returns a custom dat storage provider that provides metadata for local directory but content data from S3
    * @return {{metadata: function(file: string, opts: Object), content: function(file: string, opts: Object, archive: hyperdrive)}} - The hybrid storage provider
    */
-  provider () {
+  storage() {
     return {
       metadata: (file, opts) => this.localStorage.metadata(file, opts),
       content: (file, opts, archive) => {
